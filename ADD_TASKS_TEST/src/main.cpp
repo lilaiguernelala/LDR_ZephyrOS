bb3b300c3f2f5234e91f74988b44e5aeb877f1df
lilaiguernelala <iguernelala@et.esiea.fr>
2024-07-24 10:40:16 +0200

    Task test

--- ADD_TASKS_TEST/src/main.cpp
+++ ADD_TASKS_TEST/src/main.cpp
@@ -17,7 +17,6 @@
 #include "servo.hpp"
 
 #include <zephyr/drivers/pwm.h>
-
 #include <sensor/hc_sr04/hc_sr04.h>
 
 LOG_MODULE_REGISTER(app);
@@ -43,7 +42,7 @@ const struct device *hc_sr04_dev;
 myBME680 bme680;
 myServo servo;
 
-#define STEP PWM_USEC(100)
+static volatile bool object_detected = false;    
 
 enum direction {
     DOWN,
@@ -52,45 +51,46 @@ enum direction {
 
 static void bme680_task(void *p1, void *p2, void *p3)
 {
-	char name[10] = "BME680";
-	k_tid_t tid = k_current_get();
-	int period = PERIOD_BME680_TASK;
+    char name[10] = "BME680";
+    k_tid_t tid = k_current_get();
+    int period = PERIOD_BME680_TASK;
 
-	uint32_t start;
+    uint32_t start;
 
-	struct k_timer timer;
-	k_timer_init(&timer, NULL, NULL);
-	k_timer_start(&timer, K_MSEC(0), K_MSEC(period));
-	LOG_INF("Run task BME680 - Priority %d - Period %d\n", k_thread_priority_get(tid), period);
-	while (1)
-	{
-		k_timer_status_sync(&timer);
-		LOG_INF("START task %s", name);
-		start = k_uptime_get_32();
-		bme680.update_values();
-		LOG_INF("END task %s - %dms", name, k_uptime_get_32() - start);		
-	}
+    struct k_timer timer;
+    k_timer_init(&timer, NULL, NULL);
+    k_timer_start(&timer, K_MSEC(0), K_MSEC(period));
+   // k_msleep(1000);  /////////
+    LOG_INF("Run task BME680 - Priority %d - Period %d\n", k_thread_priority_get(tid), period);
+    while (1)
+    {
+        k_timer_status_sync(&timer);
+     //   k_msleep(1000);  ////////////
+        LOG_INF("START task %s", name);
+        start = k_uptime_get_32();
+        bme680.update_values();
+      //  k_msleep(1000);  //////////////
+        LOG_INF("END task %s - %dms", name, k_uptime_get_32() - start);       
+    }
 }
 
 static void servo_task(void *p1, void *p2, void *p3)
 {
-	char name[20] = "SERVO";
+    char name[20] = "SERVO";
 
     while (1) {
-        servo.setPosition(-90);
-        k_sleep(K_SECONDS(1));
-		LOG_INF("START task2 %s", name);
-        servo.setPosition(-45);
-        k_sleep(K_SECONDS(1));
-        servo.setPosition(0);
-        k_sleep(K_SECONDS(1));
-        servo.setPosition(45);
-        k_sleep(K_SECONDS(1));
-        servo.setPosition(90);
-        k_sleep(K_SECONDS(1));
+        if (object_detected) {
+            servo.setPosition(90);
+         ///   k_msleep(1000);
+            LOG_INF("Servo moving to 90 degrees in task %s", name);
+        } else {
+            servo.setPosition(0);
+          ///  k_msleep(1000);
+            LOG_INF("Servo moving to 0 degrees in task %s", name);
+        }
+        k_sleep(K_SECONDS(2));  
     }
 }
-
 
 void hc_sr04_task(void *p1, void *p2, void *p3)
 {
@@ -100,31 +100,39 @@ void hc_sr04_task(void *p1, void *p2, void *p3)
     while (1) {
         ret = sensor_sample_fetch(hc_sr04_dev);
         if (ret != 0) {
+           /// k_msleep(1000);
             LOG_ERR("Cannot take measurement: %d", ret);
         } else {
             sensor_channel_get(hc_sr04_dev, SENSOR_CHAN_DISTANCE, &distance);
+          ///  k_msleep(1000);
+
             LOG_INF("Distance: %d.%06d cm", distance.val1, distance.val2);
+            if (distance.val1 < 20) {      ////// INFERIEUR A 20 CM
+                object_detected = true;
+            } else {
+                object_detected = false;
+            }
         }
-        k_sleep(K_MSEC(PERIOD_HC_SR04_TASK));
+        k_sleep(K_SECONDS(2));  
     }
 }
 
 int main(void)
 {
-	struct k_thread bme680_t;
-	struct k_thread servo_t;
+    struct k_thread bme680_t;
+    struct k_thread servo_t;
     struct k_thread hc_sr04_t;
 
-	bme680.init();
+    bme680.init();
     servo.init(&servo_dev, min_pulse, max_pulse); 
-	hc_sr04_dev = DEVICE_DT_GET_ANY(zephyr_hc_sr04);
-	__ASSERT(hc_sr04_dev == NULL, "Failed to get device binding");
+    hc_sr04_dev = DEVICE_DT_GET_ANY(zephyr_hc_sr04);
+    __ASSERT(hc_sr04_dev != NULL, "Failed to get device binding");
 
-	k_thread_create(&bme680_t, bme680_stack, K_THREAD_STACK_SIZEOF(bme680_stack),
-					bme680_task, NULL, NULL, NULL,
-					PRIO_BME680_TASK, 0, K_NO_WAIT);
+    k_thread_create(&bme680_t, bme680_stack, K_THREAD_STACK_SIZEOF(bme680_stack),
+                    bme680_task, NULL, NULL, NULL,
+                    PRIO_BME680_TASK, 0, K_NO_WAIT);
 
-	k_thread_create(&servo_t, servo_stack, K_THREAD_STACK_SIZEOF(servo_stack),
+    k_thread_create(&servo_t, servo_stack, K_THREAD_STACK_SIZEOF(servo_stack),
                     servo_task, NULL, NULL, NULL,
                     PRIO_SERVO_TASK, 0, K_NO_WAIT);
 
@@ -132,10 +140,9 @@ int main(void)
                     hc_sr04_task, NULL, NULL, NULL,
                     PRIO_HC_SR04_TASK, 0, K_NO_WAIT);  
 
-	k_thread_start(&bme680_t);
+    k_thread_start(&bme680_t);
     k_thread_start(&servo_t);
-	k_thread_start(&hc_sr04_t);
+    k_thread_start(&hc_sr04_t);
 
-	return 0;
-
+    return 0;
 }
